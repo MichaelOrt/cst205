886a0de0c04e18c362d9f4a03f1faa36e1b5d24f
Wes Modes <wmodes@gmail.com>
2018-10-29 11:44:45 -0700

    reorg folders

--- argv-experiment.py
+++
@@ -1,5 +0,0 @@
-#!/Users/wmodes/dev/envs/cst205env/bin/python
-
-import sys
-
-print("Parameters are:",sys.argv)
--- hw1_task1.py
+++
@@ -1,8 +0,0 @@
-#
-# hw1_task1.py - solution to homework 1
-# author: Wes Modes
-# date: 15 Sep 2018
-#
-
-import pickle
-
